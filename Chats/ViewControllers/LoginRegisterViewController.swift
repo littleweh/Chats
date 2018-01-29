c1132f3b07fd896043de325b6564e5967be6cf8b
littleweh <adakao@gmail.com>
2018-01-29 15:03:31 +0800

    delete storyboard, and add LoginRegisterVC

--- Chats/ViewControllers/LoginRegisterViewController.swift
+++ Chats/ViewControllers/LoginRegisterViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginRegisterViewController.swift
 //  Chats
 //
 //  Created by 典萱 高 on 2018/1/29.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginRegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
-
